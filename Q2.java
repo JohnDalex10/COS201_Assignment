401eaee77df6cf7cdf0bd912979d29b8fbd3e149
John Alex <85435945+JohnDalex10@users.noreply.github.com>
2025-01-25 21:40:12 +0100

    Latest changes v1.3

--- Q2.java
+++ Q2.java
@@ -1,8 +1,8 @@
 public class Q2 {
     public static void main(String[] args) {
         System.out.println("USA's Flag (Single Loop):");
-        for (int i = 1; i <= 6; i++){
-            if (i <=3 ){
+        for (int i = 1; i<=6; i++){
+            if (i<=3){
                 System.out.println("* * * * = = = = = = =");
             } else {
                 System.out.println("= = = = = = = = = = =");
