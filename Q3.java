c709aff6a2206d09c0ef3dabecf300753f6dc56e
John Alex <85435945+JohnDalex10@users.noreply.github.com>
2025-01-25 21:38:49 +0100

    Latest changes v1.3b

--- Q3.java
+++ Q3.java
@@ -2,24 +2,24 @@ import java.util.Arrays;
 
 public class Q3 {
     public static void main(String[] args) {
-        int[] values = {2, 5, 5, 9, 4, 7, 0, 9, 6, 11, 12};
+        int[] digits = {2, 5, 5, 9, 4, 7, 0, 9, 6, 11, 12};
         
         // Calculate mean
         int sum = 0;
-        for (int num : values) sum += num;
-        double mean = (double) sum / values.length;
+        for (int num : digits) sum += num;
+        double mean = (double) sum / digits.length;
         
         // Calculate variance, then standard deviation
         double variance = 0;
-        for (int num : values) {
+        for (int num : digits) {
             variance += Math.pow(num - mean, 2);
         }
-        double stdDev = Math.sqrt(variance / values.length);
+        double stdDev = Math.sqrt(variance / digits.length);
         
         // Calculate median after sorting
-        Arrays.sort(values);
-        int mid = values.length / 2;
-        double median = values.length % 2 != 0 ? values[mid] : (values[mid-1] + values[mid]) / 2.0;
+        Arrays.sort(digits);
+        int mid = digits.length / 2;
+        double median = digits.length % 2 != 0 ? digits[mid] : (digits[mid-1] + digits[mid]) / 2.0;
         
         System.out.println("Mean: " + mean);
         System.out.println("Median: " + median);
